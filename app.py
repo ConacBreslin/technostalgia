810949225fc5164adade99f1e6ebe26636d2de89
ConacBreslin <conacbreslin@sky.com>
2021-08-28 13:41:17 +0000

    Rename technologies.html

--- app.py
+++ app.py
@@ -5,15 +5,23 @@ from flask import (
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 if os.path.exists("env.py"):
-    import envi
+    import env
 
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
+
 
 @app.route("/")
-def hello():
-    return "Hello World ... again!"
+@app.route("/get_techs")
+def get_techs():
+    techs = mongo.db.technologies.find()
+    return render_template("technologies.html", techs=techs)
 
 
 if __name__ == "__main__":
