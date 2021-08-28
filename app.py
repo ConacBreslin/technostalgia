a0aefdfcdfa4f038949fa31d03a6cf52a9604b1e
ConacBreslin <conacbreslin@sky.com>
2021-08-28 16:31:27 +0000

    Add functionality for registration

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -26,6 +27,24 @@ def get_technologies():
 
 @app.route("/registration", methods=["GET", "POST"])
 def registration():
+     if request.method == "POST":
+        # check if username already exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("registration"))
+
+        registration = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(registration)
+
+        # put the new user into 'session' cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful!")
     return render_template("registration.html")
 
 
