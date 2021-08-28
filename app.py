7e6e2af55f442df3b817ca9ed4084d9a2b8f389e
ConacBreslin <conacbreslin@sky.com>
2021-08-28 13:34:51 +0000

    Connect Flask to MongoDB

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 import os
-from flask import Flask
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
-    import env
+    import envi
 
 
 app = Flask(__name__)
