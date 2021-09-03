77afa938d551cce55b51a7079ed36637c3b17a30
ConacBreslin <conacbreslin@sky.com>
2021-09-03 14:14:07 +0000

    Tidy up formatting across the site

--- app.py
+++ app.py
@@ -8,9 +8,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
-
 app = Flask(__name__)
-
 
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
@@ -38,14 +36,14 @@ def get_technologies():
 def registration():
     if request.method == "POST":
 
-        # Check if passwords match
+        # Check if the passwords match
         user_password = request.form.get("password")
         confirm_password = request.form.get("confirm_password")
         if user_password != confirm_password:
-            flash('Passwords do not match, please try again')
+            flash('Your passwords do not match, please try again')
             return redirect(url_for("registration"))
 
-        # check if username already exists in db
+        # Check if the username is already in the database
         existing_user = mongo.db.users.find_one(
             {"username": request.form.get("username").lower()})
 
@@ -62,41 +60,43 @@ def registration():
         }
         mongo.db.users.insert_one(registration)
 
-        # put the new user into 'session' cookie
+        # Put the new user into the 'session' cookie
         session["user"] = request.form.get("username").lower()
-        flash("Welcome {}, you have successfullly registered".format(
+        flash("Welcome {}, you have successfully registered and you are now logged ".format(
                             request.form.get("username")))
         return redirect(url_for("get_technologies", username=session["user"]))
-    
-
     return render_template("registration.html", page_title="Register")
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
-        # check if username exists in db
+
+        # Check if the username exists in the database
         existing_user = mongo.db.users.find_one(
             {"username": request.form.get("username").lower()})
 
         if existing_user:
-            # ensure hashed password matches user input
+
+            # Make sure the hashed password matches the user's password
             if check_password_hash(
                     existing_user["password"], request.form.get("password")):
                         session["user"] = request.form.get("username").lower()
-                        flash("Welcome {}, you have successfully logged in".format(
-                            request.form.get("username")))
+                        flash(
+                            "Welcome {}, you have successfully logged in"
+                            .format(request.form.get("username")))
                         return redirect(url_for(
                             "get_technologies", username=session["user"]))
 
             else:
-                # invalid password match
-                flash("Incorrect Username and/or Password. Please try again")
+                # If passwords don't match
+                flash("Your Username and/or Password were incorrect. Please try again")
                 return redirect(url_for("login"))
 
         else:
-            # username doesn't exist
-            flash("Incorrect Username and/or Password. Please try again")
+
+            # If username doesn't exist in database
+            flash("Your Username and/or Password were incorrect. Please try again")
             return redirect(url_for("login"))
 
     return render_template("login.html", page_title="Log In")
@@ -104,19 +104,22 @@ def login():
 
 @app.route("/profile/<username>", methods=["GET", "POST"])
 def profile(username):
-    # grab the session user's username from db
+
+    # Get the session user's username from the database
     username = mongo.db.users.find_one(
         {"username": session["user"]})["username"]
 
     if session["user"]:
-        return render_template("profile.html", username=username, page_title="Profile")
+        return render_template(
+            "profile.html", username=username, page_title="Profile")
 
     return redirect(url_for("login"))
 
 
 @app.route("/logout")
 def logout():
-    # remove user from session cookie
+
+    # Remove the user from session cookie
     flash("You have been logged out")
     session.pop("user")
     return redirect(url_for("login"))
@@ -124,22 +127,26 @@ def logout():
 
 @app.route("/add_technology", methods=["GET", "POST"])
 def add_technology():
+
+    # Add a techonolgy to database
     if request.method == "POST":
         technology = {
             "technology_name": request.form.get("technology_name"),
             "category_name": request.form.get("category_name"),
             "technology_image": request.form.get("technology_image"),
-            "technology_description": request.form.get("technology_description"),
+            "technology_description": request.form.get(
+                "technology_description"),
             "best_bits": request.form.get("best_bits"),
             "worst_bits": request.form.get("worst_bits"),
             "posted_by": session["user"]
         }
         mongo.db.technologies.insert_one(technology)
-        flash("You have successfully {{ technology_name }} to {{ category_name}}. Thank you!")
+        flash("You have successfully added {{ technology_name }} to {{ category_name }}. Thank you!")
         return redirect(url_for("get_technologies"))
 
     categories = mongo.db.categories.find().sort("category_name", 1)
-    return render_template("add_technology.html", page_title="Add a Technology", categories=categories)
+    return render_template(
+        "add_technology.html", page_title="Add a Technology", categories=categories)
 
 
 if __name__ == "__main__":
--- static/js/script.js
+++ static/js/script.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
-    /*     jQuery code from  https://materializecss.com/ for sidebar navigation */
+// jQuery code from  https://materializecss.com/ for sidebar navigation
     $(".sidenav").sidenav({edge: "right"});
-    /*     jQuery code from  https://materializecss.com/ for home page carousel */
+// jQuery code from  https://materializecss.com/ for home page carousel */
     $('.carousel').carousel();
-/*     jQuery code from  https://materializecss.com/ for select dropdown to add technology category */
+// jQuery code from  https://materializecss.com/ for select dropdown to add technology category */
     $('select').formSelect();
 
       });
